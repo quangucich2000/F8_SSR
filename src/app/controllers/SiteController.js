79113f5338e86a7ad3daaaca9248d5b0f59381a5
quangucich2000 <lequang292k@gmail.com>
2021-05-10 11:23:34 +0700

    add mongoose, format db + connect to model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,10 +1,18 @@
+const Course = require('../models/Course')
+
 class SiteController {
-    //GET / News
+    //GET / Home
     index(req, res) {
-        res.render('home');
+        Course.find({}, function(err, courses){
+            if(!err){
+                res.json(courses)
+            }
+            else res.status(400).json({error: "co loi !!!"})
+        })
+        //res.render('home');
     }
 
-    //GET / News:slug
+    //GET / Search
     search(req, res) {
         res.render('search');
     }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes/index');
+const db = require('./config/db/index')
+
+//connect to db
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
